f9cf359918e5c0e86053776bd04cd54c3f974cdc
jpwallace22 <jpwallace22@gmail.com>
2023-01-14 18:05:12 -0800

    add more tests to utils

--- src/test/suite/utils.test.ts
+++ src/test/suite/utils.test.ts
@@ -1,20 +1,19 @@
 import * as assert from "assert";
-import { before, describe, it, suite } from "mocha";
+import { beforeEach, describe, it, suite } from "mocha";
 
 import * as vscode from "vscode";
 import * as utils from "../../utils/importUtilities";
 
 suite("Utils Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
-  before(async () => {
+  beforeEach(async () => {
     const document = await vscode.workspace.openTextDocument({
-      content:
-        "{ Fake, Modules }\nimport Fake from 'place';\nimport { Fake } from 'react';",
+      content: "import { Fake } from 'place';\nimport Fake from 'react';\n",
     });
     await vscode.window.showTextDocument(document);
   });
 
-  describe("addModuleToImport", async () => {
+  describe("addModuleToImport(module, position)", async () => {
     it("should insert module into {}", async () => {
       const editor = vscode.window.activeTextEditor;
       const start = new vscode.Position(0, 0);
@@ -24,7 +23,7 @@ suite("Utils Test Suite", () => {
 
       assert.strictEqual(
         editor?.document.lineAt(start.line).text,
-        "{ Fake, Modules, Test }"
+        "import { Fake, Test } from 'place';"
       );
     });
 
@@ -35,13 +34,13 @@ suite("Utils Test Suite", () => {
 
       assert.strictEqual(
         editor?.document.lineAt(start.line).text,
-        "import Fake, { Test } from 'place';"
+        "import Fake, { Test } from 'react';"
       );
     });
   });
 
-  describe("addNamedImport", async () => {
-    it("should add new named import at the top of the file", async () => {
+  describe("addNamedImport(module, importPath)", async () => {
+    it("Should add new named import at the top of the file", async () => {
       const editor = vscode.window.activeTextEditor;
       await utils.addNamedImport("Test", "@test-package");
 
@@ -49,6 +48,18 @@ suite("Utils Test Suite", () => {
         editor?.document.lineAt(new vscode.Position(0, 0)).text,
         "import { Test } from '@test-package';"
       );
+    });
+
+    it("Should find and add module after default import", async () => {
+      const editor = vscode.window.activeTextEditor;
+      await utils.addNamedImport("Test", "react");
+
+      setTimeout(() => {
+        assert.strictEqual(
+          editor?.document.lineAt(new vscode.Position(1, 0)).text,
+          "import Fake, { Test } from 'react';"
+        );
+      }, 1);
     });
   });
 });
